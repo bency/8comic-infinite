e8adc98a40e2ab9d9a6c4448513e29263d3adbd3
bency <bency80097@gmail.com>
2015-04-18 01:55:57 +0800

    加入 8comic 外掛 api

--- get_8comic.php
+++ get_8comic.php
@@ -5,6 +5,16 @@ $callback = $_GET['callback'];
 $url = $_GET['url'];
 $method = $_GET['method'];
 $content = iconv('big5', 'UTF-8', file_get_contents($url));
+foreach (explode(';', $content) as $data) {
+    if (preg_match("/var cs='(.*)'/i", $data, $matches)) {
+        $cs = $matches[1];
+        break;
+    }
+    if (preg_match("#\<title\>(.*)\<\/title\>#i", $data, $matches)) {
+        $title = explode(' ', $matches[1])[0];
+    }
+}
+$comic = new Comics($cs, $url, $title);
 switch($method) {
     case 'vol':
         foreach (explode(';', $content) as $data) {
@@ -16,8 +26,9 @@ switch($method) {
                 $title = explode(' ', $matches[1])[0];
             }
         }
-        echo $callback . "(" . json_encode(new Comics($cs, $url, $title)) . ");";
+        echo $callback . "(" . json_encode($comic)) . ");";
     break;
-    case 'intro':
+    case '8comic':
+        echo $callback . "({$commic->series_id}, {$comic->total_hash}, {$comic->vol});";
     break;
 }
